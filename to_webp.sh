2ef2b7a36286973899fe170268734a28e2db1691
cohenchris <40076937+cohenchris@users.noreply.github.com>
2023-10-08 18:40:53 -0700

    tweaked webp quality

--- to_webp.sh
+++ to_webp.sh
@@ -4,5 +4,5 @@
 rm -f ChrisCohen_resume.webp
 pdftoppm ChrisCohen_resume.pdf ChrisCohen_resume -png -singlefile
 
-cwebp ChrisCohen_resume.png -o ChrisCohen_resume.webp -q 1
+cwebp ChrisCohen_resume.png -o ChrisCohen_resume.webp -q 50
 rm -f ChrisCohen_resume.png
