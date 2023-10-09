c5815f92b028df3af6e5a21a5dec1bab914ba0fc
cohenchris <40076937+cohenchris@users.noreply.github.com>
2023-10-08 18:39:33 -0700

    changed to webp

--- to_webp.sh
+++ to_webp.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 # Convert resume to png for display on resume page
-rm -f ChrisCohen_resume.png
+rm -f ChrisCohen_resume.webp
 pdftoppm ChrisCohen_resume.pdf ChrisCohen_resume -png -singlefile
+
+cwebp ChrisCohen_resume.png -o ChrisCohen_resume.webp -q 1
+rm -f ChrisCohen_resume.png
