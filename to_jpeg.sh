8ae70f6a6383e7ac2639ba2f56b2a893fe419ff2
cohenchris <40076937+cohenchris@users.noreply.github.com>
2023-10-18 17:04:21 -0700

    fixed jpg generation file

--- to_jpeg.sh
+++ to_jpeg.sh
@@ -2,4 +2,4 @@
 
 # Convert resume to png for display on resume page
 rm -f ChrisCohen_resume.jpg
-pdftoppm ChrisCohen_resume.pdf ChrisCohen_resume -jpeg -singlefile
+convert ChrisCohen_resume.pdf ChrisCohen_resume.jpg
