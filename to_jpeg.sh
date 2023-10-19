3c44c68fe7b6255507c516f030b459dca8d2e8a2
cohenchris <40076937+cohenchris@users.noreply.github.com>
2023-10-18 17:03:21 -0700

    updated jpg

--- to_jpeg.sh
+++ to_jpeg.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Convert resume to png for display on resume page
-rm -f ChrisCohen_resume.jpeg
+rm -f ChrisCohen_resume.jpg
 pdftoppm ChrisCohen_resume.pdf ChrisCohen_resume -jpeg -singlefile
